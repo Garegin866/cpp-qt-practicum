3d276acbaaed9e68887bf113ff56580213fb791c
Garegin866 <gareginharutyunyan866@gmail.com>
2024-12-19 19:29:33 +0400

    Completed Calendar - horoscope app, which contains several inputs, with validations and interactions.

--- problems/sprint2/calendar-horoscope/submission/mainwindow.cpp
+++ problems/sprint2/calendar-horoscope/submission/mainwindow.cpp
@@ -8,9 +8,146 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QIntValidator* year_validator = new QIntValidator(1, 9999, this);
+    ui->le_year->setValidator(year_validator);
+
+    ui->lbl_calendar->setText(kEmptyYear);
+    this->SetHoroscopeFieldsEnabled(false);
+    ui->lbl_horoscope->setText(kDataInputDisabled);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_rb_eastern_clicked()
+{
+    UpdateCalendarLabelText();
+}
+
+void MainWindow::on_rb_tibetan_clicked()
+{
+    UpdateCalendarLabelText();
+}
+
+void MainWindow::on_rb_zoroastrian_clicked()
+{
+    UpdateCalendarLabelText();
+}
+
+void MainWindow::on_rb_zodiac_clicked()
+{
+    UpdateHoroscopeLabelText();
+}
+
+void MainWindow::on_rb_druid_clicked()
+{
+    UpdateHoroscopeLabelText();
+}
+
+void MainWindow::on_cb_is_date_clicked()
+{
+    bool enabled = ui->cb_is_date->isChecked();
+    this->SetHoroscopeFieldsEnabled(enabled);
+
+    if (!enabled) {
+        ui->lbl_horoscope->setText(kDataInputDisabled);
+    } else {
+        if (ui->le_year->text().isEmpty()) {
+            ui->lbl_horoscope->setText(kInvalidDate);
+        } else {
+            UpdateHoroscopeLabelText();
+        }
+    }
+}
+
+void MainWindow::on_le_year_textChanged(const QString &arg)
+{
+    UpdateCalendarLabelText();
+    UpdateHoroscopeLabelText();
+}
+
+void MainWindow::SetHoroscopeFieldsEnabled(bool enabled) {
+    ui->gb_horoscope->setEnabled(enabled);
+    ui->lbl_day->setEnabled(enabled);
+    ui->lbl_month->setEnabled(enabled);
+
+    ui->sb_day->setEnabled(enabled);
+    ui->cb_month->setEnabled(enabled);
+}
+
+void MainWindow::UpdateCalendarLabelText() {
+    QString year_text = ui->le_year->text();
+    if (year_text.isEmpty()) {
+        ui->lbl_calendar->setText(kEmptyYear);
+    } else {
+        int year = year_text.toInt();
+        QString lbl_calendar_text;
+        if (ui->rb_eastern->isChecked()) {
+            QString animal = GetAnimalForEasternCalendar(year);
+            QString element = GetElementForEasternCalendar(year);
+            lbl_calendar_text = kEasternLabelFormat
+                                    .arg(year)
+                                    .arg(animal)
+                                    .arg(element);
+        } else if (ui->rb_tibetan->isChecked()) {
+            QString animal = GetAnimalForTibetanCalendar(year);
+            lbl_calendar_text = kTibeZoroLabelFormat
+                                    .arg(year)
+                                    .arg(animal);
+        } else if (ui->rb_zoroastrian->isChecked()) {
+            QString animal = GetAnimalForZoroastrianCalendar(year);
+            lbl_calendar_text = kTibeZoroLabelFormat
+                                    .arg(year)
+                                    .arg(animal);
+        }
+
+        ui->lbl_calendar->setText(lbl_calendar_text);
+    }
+}
+
+void MainWindow::UpdateHoroscopeLabelText() {
+    QString lbl_horoscope_text;
+
+    int day = ui->sb_day->value();
+    int month = ui->cb_month->currentIndex();
+    int year = ui->le_year->text().toInt();
+    if (ui->rb_zodiac->isChecked()) {
+        QString zodiac = GetZodiac(day, month, year);
+        if (zodiac.isEmpty()) {
+            lbl_horoscope_text = kInvalidDate;
+        } else {
+            lbl_horoscope_text = kZodiacLabelFormat
+                                     .arg(day, 2, 10, QChar('0'))
+                                     .arg(month + 1, 2, 10, QChar('0'))
+                                     .arg(zodiac);
+        }
+    } else if (ui->rb_druid->isChecked()) {
+        QString tree = GetTree(day, month, year);
+        if (tree.isEmpty()) {
+            lbl_horoscope_text = kInvalidDate;
+        } else {
+            lbl_horoscope_text = kDruidLabelFormat
+                                     .arg(day, 2, 10, QChar('0'))
+                                     .arg(month + 1, 2, 10, QChar('0'))
+                                     .arg(GetTree(day, month, year));
+        }
+    }
+
+    ui->lbl_horoscope->setText(lbl_horoscope_text);
+}
+
+
+void MainWindow::on_sb_day_valueChanged(int arg1)
+{
+    UpdateHoroscopeLabelText();
+}
+
+
+void MainWindow::on_cb_month_currentIndexChanged(int index)
+{
+    UpdateHoroscopeLabelText();
+}
+
--- problems/sprint2/calendar-horoscope/submission/mainwindow.h
+++ problems/sprint2/calendar-horoscope/submission/mainwindow.h
@@ -18,6 +18,17 @@ enum class Horoscope {
     kDruid
 };
 
+// Error messages
+inline const QString kEmptyYear = "Введите год";
+inline const QString kDataInputDisabled = "Ввод даты отключён";
+inline const QString kInvalidDate = "Такой даты не существует";
+
+// Formats
+inline const QString kEasternLabelFormat = "%1 — это год %2. Стихия/цвет — %3";
+inline const QString kTibeZoroLabelFormat = "%1 — это год %2";
+inline const QString kZodiacLabelFormat = "%1.%2 — ваш знак зодиака %3 по зодиакальному гороскопу";
+inline const QString kDruidLabelFormat = "%1.%2 — ваше тотемное дерево %3 по гороскопу друидов";
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -27,10 +38,25 @@ public:
     ~MainWindow();
 
 private slots:
+    void on_rb_eastern_clicked();
+    void on_rb_tibetan_clicked();
+    void on_rb_zoroastrian_clicked();
+
+    void on_rb_zodiac_clicked();
+    void on_rb_druid_clicked();
+
+    void on_le_year_textChanged(const QString &arg);
+    void on_cb_is_date_clicked();
+    void on_sb_day_valueChanged(int arg1);
+    void on_cb_month_currentIndexChanged(int index);
 
 private:
     Ui::MainWindow *ui;
     Calendar calendar_ = Calendar::kEastern;
     Horoscope horoscope_ = Horoscope::kZodiac;
+
+    void SetHoroscopeFieldsEnabled(bool enabled);
+    void UpdateCalendarLabelText();
+    void UpdateHoroscopeLabelText();
 };
 #endif // MAINWINDOW_H
