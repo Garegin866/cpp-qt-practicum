cd789a1a48eb9fe1695a598c3273f97bd3d2a18b
Garegin866 <gareginharutyunyan866@gmail.com>
2025-02-03 23:24:33 +0400

    Created gui app using qt, where used lambdas and capture block.

--- problems/sprint3/magic-buttons-2/submission/mainwindow.cpp
+++ problems/sprint3/magic-buttons-2/submission/mainwindow.cpp
@@ -1,1 +1,51 @@
-// Используйте ваше решение.
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDesktopServices>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    buttons_ = {ui->pb_button_1, ui->pb_button_2, ui->pb_button_3, ui->pb_button_4};
+
+    connect(setting_, &Setting::signalSetAction, this, &MainWindow::slotShowAction);
+    setWindowFlags(Qt::WindowStaysOnTopHint);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::slotShowAction(int num, QString action_name, std::function<void()> action) {
+    buttons_[num]->setText(action_name);
+    buttons_[num]->disconnect();
+    connect(buttons_[num], &QPushButton::clicked, action);
+}
+
+void MainWindow::on_pb_sett_1_clicked()
+{
+    setting_->show();
+    setting_->SetButtonForSetting(0);
+}
+
+void MainWindow::on_pb_sett_2_clicked()
+{
+    setting_->show();
+    setting_->SetButtonForSetting(1);
+}
+
+void MainWindow::on_pb_sett_3_clicked()
+{
+    setting_->show();
+    setting_->SetButtonForSetting(2);
+}
+
+void MainWindow::on_pb_sett_4_clicked()
+{
+    setting_->show();
+    setting_->SetButtonForSetting(3);
+}
--- problems/sprint3/magic-buttons-2/submission/setting.cpp
+++ problems/sprint3/magic-buttons-2/submission/setting.cpp
@@ -1,1 +1,45 @@
-// Используйте ваше решение.
+#include "setting.h"
+#include "ui_setting.h"
+
+#include <prac/QInputDialog>
+
+Setting::Setting(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Setting)
+{
+    ui->setupUi(this);
+
+    setWindowFlags(Qt::WindowStaysOnTopHint);
+
+    auto OpenCustomUrlAction = [&]() {
+        QString user_input = prac::QInputDialog::getText(this, "Введите URL", "URL:");
+        OpenCustomUrl(user_input);
+    };
+
+    auto OpenCustomDirectoryAction = [&]() {
+        QString user_input = prac::QInputDialog::getText(this, "Введите путь к папке", "Путь:");
+        OpenCustomDirectory(user_input);
+    };
+
+    actions_.push_back({"Открыть URL", OpenCustomUrlAction});
+    actions_.push_back({"Открыть папку", OpenCustomDirectoryAction});
+
+    for (const auto& action : actions_) {
+        ui->cb_action->addItem(action.name);
+    }
+}
+
+Setting::~Setting() {
+    delete ui;
+}
+
+void Setting::on_pb_save_clicked() {
+    auto index = ui->cb_action->currentIndex();
+    if (index < 0 || static_cast<size_t>(index) >= actions_.size() + 2) {
+        return;
+    }
+
+    emit signalSetAction(current_button_idx_, actions_[index].name, actions_[index].action);
+    ui->cb_action->setCurrentIndex(0);
+    this->close();
+}
